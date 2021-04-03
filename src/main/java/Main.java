87c1f1ce3c8b78327288b56bbf947130fc758842
Kortique <kolobOK8>
2021-04-03 20:31:25 +0300

    ДЗ 1 урока. Воронич Иван.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,14 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Сегодня %tA %<te %<tB %<tY года.%nВыполняется ДЗ №1%n%n", new Date());
+        System.out.printf("Сегодня %tA %<te %<tB %<tY года.%nВыполняется ДЗ №1.%n%n", new Date());
         testVars();
-        System.out.println("Задание 3: " + sumArgs(50,20,30,40));
-        System.out.println("Задание 4: " + checkSumArgs(12,3));
+        System.out.println("Задание 3: " + sumArgs(50, 20, 30, 40));
+        System.out.println("Задание 4: " + checkSumArgs(12, 3));
         checkNumber(-5);
+        System.out.println("Задание 6: " + checkNumberBool(-100));
+        sayHello("Сергей");
+        checkLeapYear(104);
     }
 
     public static void testVars() {
@@ -32,10 +35,29 @@ public class Main {
     }
 
     public static void checkNumber(int a) {
-        if (a>= 0) {
+        if (a >= 0) {
             System.out.println("Задание 5: передали положительное число");
         } else {
             System.out.println("Задание 5: передали отрицательное число");
+        }
+    }
+
+    public static boolean checkNumberBool(int a) {
+        return !(a >= 0);
+    }
+
+    public static void sayHello(String name) {
+        System.out.printf("Задание 7: Привет, %s!%n", name);
+    }
+
+    public static void checkLeapYear(int a) {
+        int LeapYear004 = a % 4;
+        int LeapYear100 = a % 100;
+        int LeapYear400 = a % 400;
+        if (LeapYear400 == 0 || (LeapYear004 == 0 && LeapYear100 != 0)) {
+            System.out.printf("Задание 8: %d год - високосный%n", a);
+        } else {
+            System.out.printf("Задание 8: %d год - невисокосный%n", a);
         }
     }
 
