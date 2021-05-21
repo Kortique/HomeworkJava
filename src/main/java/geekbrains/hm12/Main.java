33b32f05d1892ce1d5010b6ccecb488e1d257507
Kortique <kolobOK8>
2021-05-21 13:04:10 +0300

    ДЗ №12 исправленная (реализовано совпадение значений массивов из разных методов)

--- src/main/java/geekbrains/hm12/Main.java
+++ src/main/java/geekbrains/hm12/Main.java
@@ -19,24 +19,25 @@ public class Main {
 
     private static void startSingleThread(float[] array) {
         long a = System.currentTimeMillis();
-        calculate(array);
+        float[] array1 = Arrays.copyOfRange(array, 0, SIZE);
+        calculate(array1, 0);
+//        System.out.println(Arrays.toString(array1));
         System.out.println("Время выполнения для 1 потока " + (System.currentTimeMillis() - a) + "мс");
     }
 
-    private static void calculate(float[] array) {
-        for (int i = 0; i < array.length-1; i++) {
-            array[i] = (float) (array[i] * Math.sin(0.2f + i / 5) *
-                    Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    private static void calculate(float[] array, int add) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (float) (array[i] * Math.sin(0.2f + (i + add) / 5) *
+                    Math.cos(0.2f + (i + add) / 5) * Math.cos(0.4f + (i + add) / 2));
         }
     }
 
     private static void startDoubleThreads(float[] array) {
         long a = System.currentTimeMillis();
-
         float[] array1 = Arrays.copyOfRange(array, 0, H);
         float[] array2 = Arrays.copyOfRange(array, H, SIZE);
-        Thread thread1 = new Thread(() -> calculate(array1));
-        Thread thread2 = new Thread(() -> calculate(array2));
+        Thread thread1 = new Thread(() -> calculate(array1, 0));
+        Thread thread2 = new Thread(() -> calculate(array2, H));
         thread1.start();
         thread2.start();
         try {
@@ -48,10 +49,9 @@ public class Main {
         float[] arrayOriginal = new float[SIZE];
             System.arraycopy(array1, 0, arrayOriginal, 0, array1.length);
             System.arraycopy(array2, 0, arrayOriginal, array1.length, array2.length);
+//        System.out.println(Arrays.toString(arrayOriginal));
 
         System.out.println("Время выполнения для 2 потоков " + (System.currentTimeMillis() - a) + "мс");
-
-//        System.out.println("длина объединённого массива: " + arrayOriginal.length);
 
     }
 
@@ -62,10 +62,10 @@ public class Main {
         float[] array2 = Arrays.copyOfRange(array, H/2, H);
         float[] array3 = Arrays.copyOfRange(array, H, SIZE-H/2);
         float[] array4 = Arrays.copyOfRange(array, SIZE-H/2, SIZE);
-        Thread thread1 = new Thread(() -> calculate(array1));
-        Thread thread2 = new Thread(() -> calculate(array2));
-        Thread thread3 = new Thread(() -> calculate(array3));
-        Thread thread4 = new Thread(() -> calculate(array4));
+        Thread thread1 = new Thread(() -> calculate(array1, 0));
+        Thread thread2 = new Thread(() -> calculate(array2, H/2));
+        Thread thread3 = new Thread(() -> calculate(array3, H));
+        Thread thread4 = new Thread(() -> calculate(array4, SIZE-H/2));
         thread1.start();
         thread2.start();
         thread3.start();
@@ -85,8 +85,6 @@ public class Main {
         System.arraycopy(array4, 0, arrayOriginal, array3.length, array4.length);
 
         System.out.println("Время выполнения для 4 потоков " + (System.currentTimeMillis() - a) + "мс");
-
-//        System.out.println("длина объединённого массива: " + arrayOriginal.length);
 
     }
 
