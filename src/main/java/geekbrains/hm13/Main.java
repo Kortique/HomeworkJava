be3f0b97f3747ec1444c3aa5c52e0b6797bf8660
Kortique <kolobOK8>
2021-05-24 19:42:48 +0300

    ДЗ №13

--- src/main/java/geekbrains/hm13/Main.java
+++ src/main/java/geekbrains/hm13/Main.java
@@ -26,6 +26,7 @@ public class Main {
         } catch (InterruptedException | BrokenBarrierException e) {
             e.printStackTrace();
         }
+
         System.out.println("ПОБЕДИТЕЛЬ  - " + Car.getWinner() + "!!!");
     }
 }
